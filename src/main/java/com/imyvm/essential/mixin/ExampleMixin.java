1265c70e808e5d8aaa4110a0342b2a231aa59fd8
YanWQ-monad <YanWQmonad@gmail.com>
2022-06-17 13:41:30 +0800

    init: initial commit

---
+++ src/main/java/com/imyvm/essential/mixin/ExampleMixin.java
@@ -0,0 +1,15 @@
+package com.imyvm.essential.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		com.imyvm.essential.EssentialMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
