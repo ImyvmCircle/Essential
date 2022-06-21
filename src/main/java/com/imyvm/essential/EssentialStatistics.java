8fadddf9bb29d78e565507f99df2d04fbb5ff167
张斌 <zhlinos@hotmail.com>
2022-06-22 04:03:52 +0800

    feat: add creeper griefing rules

--- src/main/java/com/imyvm/essential/EssentialStatistics.java
+++ src/main/java/com/imyvm/essential/EssentialStatistics.java
@@ -1,14 +1,21 @@
 package com.imyvm.essential;
 
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.stat.StatFormatter;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.GameRules;
 
 public class EssentialStatistics {
     // The total amount of time played (tracked in seconds)
     public static final Identifier PLAY_TIME_TRACK;
+
+    // creeper rules
+    public static GameRules.Key<GameRules.BooleanRule> creeperGriefing;
+    public static GameRules.Key<GameRules.BooleanRule> creeperFire;
 
     // Force JVM to initialize the static fields
     public static void initialize() {}
@@ -22,5 +29,7 @@ public class EssentialStatistics {
 
     static {
         PLAY_TIME_TRACK = register("play_time_track", StatFormatter.DEFAULT);
+        creeperGriefing = GameRuleRegistry.register("creeperGriefing", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
+        creeperFire = GameRuleRegistry.register("creeperFire", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(false));
     }
 }
