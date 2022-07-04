6562cf2bcbdbb6bf4b38f05da3109bd1da5a5cc3
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-04 22:26:38 +0800

    refactor(fire): use `Inject` instead of `Redirect`

--- src/main/java/com/imyvm/essential/mixin/LightningEntityMixin.java
+++ src/main/java/com/imyvm/essential/mixin/LightningEntityMixin.java
@@ -4,14 +4,15 @@ import com.imyvm.essential.EssentialGameRules;
 import net.minecraft.entity.LightningEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LightningEntity.class)
 public class LightningEntityMixin {
-    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LightningEntity;spawnFire(I)V"))
-    private void redirectSpawnFire(LightningEntity entity, int spreadAttempts) {
-        if (entity.world.getGameRules().getBoolean(EssentialGameRules.DO_LIGHTNING_SPAWN_FIRE)) {
-            ((LightningEntityInvoker) entity).imyvm$spawnFire(spreadAttempts);
-        }
+    @Inject(method = "spawnFire", at = @At("HEAD"), cancellable = true)
+    private void shouldSpawnFire(int spreadAttempts, CallbackInfo ci) {
+        LightningEntity self = (LightningEntity) (Object) this;
+        if (!self.getWorld().getGameRules().getBoolean(EssentialGameRules.DO_LIGHTNING_SPAWN_FIRE))
+            ci.cancel();
     }
 }
