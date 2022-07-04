1e655708a9e35783c9e4eda77cd9667207b8d887
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-04 17:45:28 +0800

    refactor(afk): remove unused error message

--- src/main/java/com/imyvm/essential/commands/AfkCommand.java
+++ src/main/java/com/imyvm/essential/commands/AfkCommand.java
@@ -6,16 +6,10 @@ import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 
-import static com.imyvm.essential.Translator.tr;
-
 public class AfkCommand extends BaseCommand {
     @Override
     public int run(CommandContext<ServerCommandSource> context) {
         ServerPlayerEntity player = context.getSource().getPlayer();
-        if (player == null) {
-            context.getSource().sendError(tr("commands.afk.failed.not_player"));
-            return 0;
-        }
 
         PlayerEntityMixinInterface playerMixin = (PlayerEntityMixinInterface) player;
         boolean status = playerMixin.imyvm$isAwayFromKeyboard();
