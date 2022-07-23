75587ea71cdeb69788e13d272ec30f9b1e4d1834
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-23 17:18:30 +0800

    feat: print log when an allay try to duplicate item

--- src/main/java/com/imyvm/essential/mixin/GiveInventoryToLookTargetTaskMixin.java
+++ src/main/java/com/imyvm/essential/mixin/GiveInventoryToLookTargetTaskMixin.java
@@ -1,18 +1,45 @@
 package com.imyvm.essential.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.LookTarget;
 import net.minecraft.entity.ai.brain.task.GiveInventoryToLookTargetTask;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Surrogate;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Optional;
+
+import static com.imyvm.essential.EssentialMod.LOGGER;
 
 @Mixin(GiveInventoryToLookTargetTask.class)
 public class GiveInventoryToLookTargetTaskMixin<E extends LivingEntity> {
-    @Inject(method = "keepRunning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;J)V", at = @At("HEAD"), cancellable = true)
-    private void skipRunningWhenRemoved(ServerWorld world, E entity, long time, CallbackInfo ci) {
-        if (entity.isRemoved())
+    @Inject(
+        method = "keepRunning(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;J)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/brain/task/GiveInventoryToLookTargetTask;playThrowSound(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/math/Vec3d;)V"),
+        cancellable = true,
+        locals = LocalCapture.CAPTURE_FAILHARD)
+    private void skipThrowingWhenRemoved(ServerWorld world, E entity, long time, CallbackInfo ci, Optional<LookTarget> optional, LookTarget lookTarget, double distance, ItemStack itemStack) {
+        if (entity.isRemoved()) {
+            logSuspiciousBehaviour(entity, itemStack);
             ci.cancel();
+        }
+    }
+
+    @Surrogate
+    private void skipThrowingWhenRemoved(ServerWorld world, E entity, long time, CallbackInfo ci) {
+        if (entity.isRemoved()) {
+            logSuspiciousBehaviour(entity, null);
+            ci.cancel();
+        }
+    }
+
+    private void logSuspiciousBehaviour(LivingEntity entity, ItemStack itemStack) {
+        String item = itemStack == null ? "[unknown]" : itemStack.getItem().toString();
+        LOGGER.warn("Allay" + entity.getPos() + " try to duplicate " + item);
     }
 }
