4825eac6913c5ad4dc96db75e1150f6ef1995510
YanWQ-monad <YanWQmonad@gmail.com>
2022-07-23 18:11:52 +0800

    refactor: add namespace to some config options

--- src/main/java/com/imyvm/essential/ModConfig.java
+++ src/main/java/com/imyvm/essential/ModConfig.java
@@ -30,9 +30,9 @@ public class ModConfig extends HokiConfig {
     @ConfigOption public final Option<String> FLY_LIFETIME_ICON = new Option<>("fly.icon.lifetime", "minecraft:diamond", "The icon of lifetime flying, represented by Minecraft item identifier", Config::getString);
     @ConfigOption public final Option<String> FLY_CANCEL_ICON = new Option<>("fly.icon.cancel", "minecraft:barrier", "The icon of cancelling, represented by Minecraft item identifier", Config::getString);
 
-    @ConfigOption public final Option<Boolean> DO_CREEPER_GRIEFING = new Option<>("doCreeperGriefing", true, "If disabled, creeper explosions do not break blocks", Config::getBoolean);
-    @ConfigOption public final Option<Boolean> DO_LIGHTNING_SPAWN_FIRE = new Option<>("doLightningSpawnFire", true, "If disabled, lightnings do not spawn fire", Config::getBoolean);
-    @ConfigOption public final Option<Boolean> DO_LAVA_SPREAD_FIRE = new Option<>("doLavaSpreadFire", true, "If disabled, lava does not spread fire", Config::getBoolean);
+    @ConfigOption public final Option<Boolean> DO_CREEPER_GRIEFING = new Option<>("tweak.doCreeperGriefing", true, "If disabled, creeper explosions do not break blocks", Config::getBoolean);
+    @ConfigOption public final Option<Boolean> DO_LIGHTNING_SPAWN_FIRE = new Option<>("tweak.doLightningSpawnFire", true, "If disabled, lightnings do not spawn fire", Config::getBoolean);
+    @ConfigOption public final Option<Boolean> DO_LAVA_SPREAD_FIRE = new Option<>("tweak.doLavaSpreadFire", true, "If disabled, lava does not spread fire", Config::getBoolean);
 
     @ConfigOption public final Option<Boolean> FIX_ALLAY_DUPLICATE_ITEM = new Option<>("fix.allay_duplicate_item", true, "Whether to fix allay duplicating item when it goes through the nether portal", Config::getBoolean);
 }
